240f1a241bece333e379127c9943d3ed513a513b
Facebook Api <llavillaccama@gmail.com>
2014-12-09 18:34:09 -0500

    add styles

--- app/views/account/signin.blade.php
+++ app/views/account/signin.blade.php
@@ -3,23 +3,25 @@
 
 <form action="{{ URL::route('account-sign-in-post') }}" method="post">
 	
-<div class="field">
-	Email: <input type="text" name="email"{{(Input::old('email')) ? 'value="'. Input::old('email').'"' :''}}>
-	@if($errors->has('email'))
-	{{ $errors->first('email') }}
-	@endif
-</div>
-<div class="field">
-	Password: <input type="password" name="password">
-	@if($errors->has('password'))
-	{{ $errors->first('password') }}
-	@endif
-</div>
-<div class="field">
-	<input type="checkbox" name="remember" id="remember">
-	<label for="remember">Remember me</label>
-</div>
-<input type="submit" value="Sign in">
-{{ Form::token() }}
+	<div class="field">
+	<label for="email">Email: </label>
+		<input type="text" name="email"{{(Input::old('email')) ? 'value="'. Input::old('email').'"' :''}}>
+		@if($errors->has('email'))
+		{{ $errors->first('email') }}
+		@endif
+	</div>
+	<div class="field">
+		<label for="password">Password:</label>
+		<input type="password" name="password">
+		@if($errors->has('password'))
+		{{ $errors->first('password') }}
+		@endif
+	</div>
+	<div class="field">
+		<input type="checkbox" name="remember" id="remember">
+		<label for="remember">Remember me</label>
+	</div>
+	<input type="submit" value="Sign in" class="btn">
+	{{ Form::token() }}
 </form>
 @stop
--- app/views/layout/navigation.blade.php
+++ app/views/layout/navigation.blade.php
@@ -1,14 +1,10 @@
 <nav>
 	<ul>
-		<li><a href="{{ URL::route('home') }}">Home</a>	</li>
 		@if(Auth::check())
-			<li><a href="/sign-out">Sign Out</a></li>
-			<li><a href="{{ URL::route('account-change-password') }}">Change password</a></li>
+		<li><a href="{{ URL::route('account-change-password') }}" class="btn btn-primary">Change password</a></li>
 		@else
-		<li><a href="{{ URL::route('account-sign-in') }}">Sign in</a></li>
-		<li><a href="{{ URL::route('account-create') }}">Create an account</a></li>
-		<li><a href="{{ URL::route('account-forgot-password') }}">Forgot password</a></li>
-		
+		<li><a href="{{ URL::route('account-create') }}" class="btn btn-primary">Create an account</a></li>
+		<li><a href="{{ URL::route('account-forgot-password') }}" class="btn btn-primary">Forgot password</a></li>
 		@endif
 	</ul>
 </nav>
