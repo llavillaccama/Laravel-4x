9537514a23c015b005b2b6a27d9bcd45bb6b8a86
Facebook Api <llavillaccama@gmail.com>
2014-12-10 18:06:30 -0500

    add database

--- database/auth(1).sql
+++ database/auth(1).sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-12-2014 a las 17:46:26
+-- Tiempo de generación: 11-12-2014 a las 00:05:43
 -- Versión del servidor: 5.6.21
 -- Versión de PHP: 5.6.3
 
@@ -27,7 +27,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL,
+`id` int(11) NOT NULL,
   `email` varchar(50) NOT NULL,
   `username` varchar(20) NOT NULL,
   `password` varchar(60) NOT NULL,
@@ -37,17 +37,35 @@ CREATE TABLE IF NOT EXISTS `users` (
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   `remember_token` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `users`
 --
 
 INSERT INTO `users` (`id`, `email`, `username`, `password`, `password_temp`, `code`, `active`, `created_at`, `updated_at`, `remember_token`) VALUES
-(0, 'llavillaccama@gmail.com', 'llavillaccama', '$2y$10$nm/H/OlD/pfKB3EcatpbGOyWdG7fIOGtWAcJScLirKPn5LRLpwCpK', '', '', 1, '2014-12-04 21:07:49', '2014-12-05 14:31:07', 'MHQRoZWACzeqJcLZIkRSMG8lTkMQ71SBgCDx260SpF5Cu2MVje7A39aQwGMi'),
-(0, 'pilar.romero07@yahoo.com', 'pilar', '$2y$10$ezb4YSyL/FA7UcFsHZ/pueNLKQ0IgweMWl59TUhBK4wwVTI/X9ELS', '', '', 1, '2014-12-04 23:06:12', '2014-12-05 14:31:07', 'MHQRoZWACzeqJcLZIkRSMG8lTkMQ71SBgCDx260SpF5Cu2MVje7A39aQwGMi'),
-(0, 'pilar@pimientadigital.com', 'pilar07', '$2y$10$6PHEDcUIeg51Aelmubc.kuUEhW7BTbYW/ae2tASK6DTEZJPnrHhqW', '', '', 1, '2014-12-04 23:08:50', '2014-12-05 14:31:07', 'MHQRoZWACzeqJcLZIkRSMG8lTkMQ71SBgCDx260SpF5Cu2MVje7A39aQwGMi');
+(3, 'llavillaccama@gmail.com', 'cesar', '$2y$10$IWgq7CKQi4.AahYcP/ZAWOMkcCm23xbW2DM.7k1uN0PBEiI7uN6L.', '', '', 1, '2014-12-09 20:32:09', '2014-12-10 16:04:05', 'cqoPS4duN2tUCT1dz7hdXgd4O0rRRPUUmjwCwTEl4ftuEtcaUXS7y87bFQN1'),
+(4, 'reina@gmail.com', 'reyna', '$2y$10$hWm2fexUuryNorNoxa881OcLJAlCrfeReu99YnSDIw4ty1a7slnlm', '', 'CassAiYoWZM10weQFmJNnL56gT5U0TNhyyvAC2Sqv8JEGWYiR9NjRVzcTfP4', 0, '2014-12-09 20:33:32', '2014-12-09 20:33:32', '');
 
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
